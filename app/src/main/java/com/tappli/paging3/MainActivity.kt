f3cef6e47ca9fe99cd7f14d84563a2bdab3aea14
Tetsuro Nakamura <t2low.nk@gmail.com>
2020-06-15 01:38:05 +0900

    とりあえずFlowに変換できるようにする

--- app/src/main/java/com/tappli/paging3/MainActivity.kt
+++ app/src/main/java/com/tappli/paging3/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.tappli.paging3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun getStream(): Flow<PagingData<String>> {
+        // 実際はRepositoryがFlowにして返す？
+        // https://codelabs.developers.google.com/codelabs/android-paging/#5
+        return Pager(config = PagingConfig(pageSize = 10), initialKey = 0) { DataSource() }.flow
     }
 }
