e4fc1dff9d197370bf30e96968a53f48ab61b985
Tetsuro Nakamura <t2low.nk@gmail.com>
2020-06-15 02:10:37 +0900

    ActivityにRecyclerViewをおいてデータを紐付け

--- app/src/main/java/com/tappli/paging3/MainActivity.kt
+++ app/src/main/java/com/tappli/paging3/MainActivity.kt
@@ -2,15 +2,28 @@ package com.tappli.paging3
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val adapter = SampleDataAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listView.adapter = adapter
+        lifecycleScope.launch {
+            getStream().collectLatest {
+                adapter.submitData(it)
+            }
+        }
     }
 
     private fun getStream(): Flow<PagingData<String>> {
