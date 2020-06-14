d6e17645e0ce5f93c00f2c701ce4f3249da693f5
Tetsuro Nakamura <t2low.nk@gmail.com>
2020-06-15 02:15:04 +0900

    少しデータを変えてみる

--- app/src/main/java/com/tappli/paging3/DataSource.kt
+++ app/src/main/java/com/tappli/paging3/DataSource.kt
@@ -6,9 +6,9 @@ import kotlinx.coroutines.delay
 class DataSource : PagingSource<Int, String>() {
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, String> {
         val pageNumber = params.key ?: 0
-        val items = (0 until 10).map { it + pageNumber * 10 }.map { it.toString() }
+        val items = (0 until 25).map { it + pageNumber * 25 }.map { it.toString() }
         val prev: Int? = if (pageNumber > 0) pageNumber - 1 else null
-        val next = pageNumber + 1
+        val next = if (pageNumber < 10) pageNumber + 1 else null
 
         delay(300)
 
